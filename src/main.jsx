4b9a30f617134d285f71e56a3cbdbc9ab92c1f67
bettyego <nwabethroseonuoha@gmail.com>
2025-07-02 05:49:48 +0100

    Comprehensive 404 fix: Updated vercel.json with v2 config, added vercel-build script, fixed React imports

--- src/main.jsx
+++ src/main.jsx
@@ -4,10 +4,11 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
-import "../tailwind.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
 );
